f44f4a49c9da06fdbc9c4625b646d7a9630645b1
PaoH2 <167370091+PaoH2@users.noreply.github.com>
2025-11-29 20:58:15 -0600

    3
    3

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  // USA LA URL REAL DE RENDER AQUÍ (Ej: https://gestor-backend-api.onrender.com/api)
+  apiUrl: 'https://gestor-sci-backend.onrender.com/api'
+};
