6cc948bf3c19dfc33800cca2d9f06f87c942f8db
PaoH2 <167370091+PaoH2@users.noreply.github.com>
2025-12-03 17:53:23 -0600

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
-import { NgModule, Component } from '@angular/core';
-import { RouterModule, Routes, CanActivate } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-// Componentes
 import { HomeComponent } from './screens/home/home.component';
 import { CrearProductoComponent } from './screens/crear-producto/crear-producto.component';
 import { ListaComponent } from './screens/lista/lista.component';
@@ -14,34 +13,31 @@ import { RegistroEntradaComponent } from './screens/registro-entrada/registro-en
 import { RegistroSalidasComponent } from './screens/registro-salidas/registro-salidas.component';
 import { HistorialMovimientosComponent } from './screens/historial-movimientos/historial-movimientos.component';
 import { PosComponent } from './screens/pos/pos.component';
-
-// Guards
-import { AuthGuard } from './guards/auth.guard';
 import { HistorialVentasComponent } from './screens/historial-ventas/historial-ventas.component';
 import { MinimoStockComponent } from './screens/minimo-stock/minimo-stock.component';
+
+import { AuthGuard } from './guards/auth.guard';
+
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full'},
   { path: 'login', component: LoginScreenComponent, pathMatch: 'full'},
 
-  // Rutas Protegidas
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
   { path: 'listap', component: ListaComponent, canActivate: [AuthGuard]},
   { path: 'entradas', component: RegistroEntradaComponent, canActivate: [AuthGuard]},
   { path: 'salidas', component: RegistroSalidasComponent, canActivate: [AuthGuard]},
   { path: 'historial', component: HistorialMovimientosComponent, canActivate: [AuthGuard]},
 
-  // Ruta POS (Nueva)
   { path: 'pos', component: PosComponent, canActivate: [AuthGuard]},
 
-  // Rutas de Superadmin
   { path: 'crear', component: CrearProductoComponent, canActivate: [AuthGuard], data: {role: 'Superadmin'}},
   { path: 'editarp/:sku', component: EditarProductoComponent, canActivate: [AuthGuard], data: {role: 'Superadmin'}},
   { path: 'gestionarusuarios', component: GestionarUsuariosComponent, canActivate: [AuthGuard], data: {role: 'Superadmin'}},
   { path: 'admin/usuarios/editar/:id', component: EditarUsuarioComponent, canActivate: [AuthGuard], data: {role: 'Superadmin'}},
   { path: 'crearusuario', component: CrearUsuarioComponent, canActivate: [AuthGuard], data: {role: 'Superadmin'}},
   { path: 'ventas', component: HistorialVentasComponent, canActivate: [AuthGuard] },
-  { path: 'stock-min', component: MinimoStockComponent, CanActivate: [AuthGuard], data: {role: 'Superadmin'}}
+  { path: 'stock-min', component: MinimoStockComponent, canActivate: [AuthGuard], data: {role: 'Superadmin'}}
 ];
 
 @NgModule({
