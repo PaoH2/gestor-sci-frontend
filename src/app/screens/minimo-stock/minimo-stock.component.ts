17b10c467727f08ec3f34124c119f5a9b2520aea
PaoH2 <167370091+PaoH2@users.noreply.github.com>
2025-12-03 18:04:42 -0600

    ok
    ok

--- src/app/screens/minimo-stock/minimo-stock.component.ts
+++ src/app/screens/minimo-stock/minimo-stock.component.ts
@@ -9,9 +9,11 @@ import { Location } from '@angular/common';
   templateUrl: './minimo-stock.component.html',
   styleUrls: ['./minimo-stock.component.scss']
 })
+
 export class MinimoStockComponent implements OnInit{
 
-  minimoForm!: FormGroup; 
+  minimoForm!: FormGroup;
+  mensajeExito: string = '';
   mensajeError: string = '';
   isLoading: boolean = true;
 
@@ -32,7 +34,6 @@ export class MinimoStockComponent implements OnInit{
       Nombre_Producto: [{ value: '', disabled: true }],
       Nivel_Minimo_Stock: [0, [Validators.required, Validators.min(0)]]
       });
-
     this.productoSKU = this.route.snapshot.paramMap.get('sku');
 
     if (this.productoSKU) {
@@ -60,7 +61,6 @@ export class MinimoStockComponent implements OnInit{
 
     this.mensajeExito = '';
     this.mensajeError = '';
-
     const nivelMinimo = this.minimoForm.get('Nivel_Minimo_Stock')?.value;
     const datosActualizados = { Nivel_Minimo_Stock: nivelMinimo };
 
@@ -74,7 +74,14 @@ export class MinimoStockComponent implements OnInit{
     });
   }
 
+
+
   goBack(): void {
     this.location.back();
   }
+
+ 
+
+
+
 }
